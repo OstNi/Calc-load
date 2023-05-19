977a389b86287d336644e82157f6878f9c0505f9
NickOstashov <126785534+OstNi@users.noreply.github.com>
2023-05-19 17:15:31 +0500

    + TyPeriodExc, NonUniqKey

---
+++ exceptions.py
@@ -0,0 +1,11 @@
+"""Exceptions"""
+
+
+class TyPeriodExc(Exception):
+    """Ошибка связанная с TyPeriod"""
+    pass
+
+
+class NonUniqKey(Exception):
+    """Добавление неуникального ключа в словарь"""
+    pass
